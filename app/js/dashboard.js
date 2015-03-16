bde90dc479bea6b76cc1b67bbd97a2deacadb9c6
J.Marx <jennyrutledge@gmail.com>
2015-03-16 16:47:56 -0400

    Can view all images in the database.

--- app/js/dashboard.js
+++ app/js/dashboard.js
@@ -7,13 +7,16 @@ var trace = function(){
 };
 
 var Dashboard = (function(){
-  var authToken, apiHost
+  var authToken, apiHost;
+  var bucketUrl = "https://s3.amazonaws.com/datingapp-wdi/uploads/";
+  var apiHost = 'http://localhost:3000/';
 
   var run = function(){
     authToken = localStorage.getItem('authToken');
-    apiHost = 'http://localhost:3000/';
     Registration.setupAjaxRequests();
     getAmazonJson();
+    getMatchesImages();
+
   };
 
   var getAmazonJson = function(){
@@ -26,13 +29,47 @@ var Dashboard = (function(){
        $('#container').html(template({
          imageHeader: data
        }));
-       getMatchesImages(data)
+       submitForm(data.key)
     }).fail(function(jqXHR, textStatus, errorThrow){
           trace(jqXHR, textStatus, errorThrow);
     });
   };
+
+  var submitForm = function(fileName){
+    var $form = $('form#imageForm');
+  $('body').on('submit',$form, function(e,$form){
+    debugger;
+      e.preventDefault();
+      postImageRails(fileName);
+      $($form).submit();
+    });
+  };
+
+  var postImageRails = function(imageUrl){
+    //need to grab profile_id from somewhere.
+    debugger;
+    var profile_id = 1;
+    $.ajax({
+      url: apiHost + 'profiles/'+ profile_id +'/images',
+      type: 'POST',
+      data: {
+        images:
+        {
+          url: bucketUrl + imageUrl,
+          profile_id: profile_id
+        }
+      }
+    }).done(function(response){
+      debugger;
+      console.log(response)
+    }).fail(function(jqXHR, textStatus, errorThrow){
+          trace(jqXHR, textStatus, errorThrow);
+    }).always(function(response){
+          trace(response);
+    });
+  }
   // currently showing all images
-  var getMatchesImages = function(header){
+  var getMatchesImages = function(){
     $.ajax({
       url: apiHost + 'images',
       type: 'GET'
@@ -56,9 +93,12 @@ var Dashboard = (function(){
     $('#matches').append('<img src="' + etag.url + '">')
     });
   };
-
-
   return{
     run:run
   };
 })();
+
+// $(document).ready(function()
+// {
+
+// });
